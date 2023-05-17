4a78df63e50822c402a0bb18a64a4b73968f342c
adamrouss <aslan.tesaev@yandex.ru>
2023-05-17 14:29:19 +0300

    sprint 11.3

--- ImageFeed/Tab Bar Controller/TabBarController.swift
+++ ImageFeed/Tab Bar Controller/TabBarController.swift
@@ -4,9 +4,22 @@
 //
 //  Created by AdamRouss on 16.05.2023.
 //
-
 import UIKit
- 
-final class TabBarController: UITabBarController {
 
+final class TabBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        
+        let imagesListViewController = storyboard.instantiateViewController(
+            withIdentifier: "ImagesListViewController"
+        )
+        
+        let profileViewController = ProfileViewController()
+        profileViewController.tabBarItem = UITabBarItem(title: nil,
+                                                        image: UIImage(named: "tab_profile_active"),
+                                                        selectedImage: nil)
+        //profileViewController.view.backgroundColor = UIColor(hex: 0x1A1B22)
+        self.viewControllers = [imagesListViewController, profileViewController]
+    }
 }
